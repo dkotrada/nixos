dec771b4a704027c0e04e73ca7787525b84643fb
Dieter Konrad <dkotrada@gmail.com>
2019-02-12 21:21:55 +0100

    Github link

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,12 @@ module.exports = {
   description: "Nix Package Manger - User friendly documenation",
   base: "/nixos/",
   themeConfig: {
+
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'dkotrada/nixos',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'Contribute!',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
