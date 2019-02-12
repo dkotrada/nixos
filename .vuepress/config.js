012346dfab28f845add3f4053a55e381752e4901
Dieter Konrad <dkotrada@gmail.com>
2019-02-12 21:56:58 +0100

    config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,17 +8,15 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Deutsch', link: '/de/' },
-      { text: 'External', link: 'https://google.com' },
+      { text: 'Contribute', link: 'https://github.com/dkotrada/nixos' },
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Group1', items: [/*  */] },
+          { text: 'Group2', items: [/*  */] }
+        ]
+      },
     ],
-
-
-    // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'dkotrada/nixos',
-    // Customising the header label
-    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
-    repoLabel: 'Contribute!',
-
-
 
   }
 }
