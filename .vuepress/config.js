824f0ab197a61b92b0242ba8b21f0f536ef64c8b
Dieter Konrad <dkotrada@gmail.com>
2019-02-12 21:47:16 +0100

    navbar test

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,12 +3,17 @@ module.exports = {
   description: "Nix Package Manger - User friendly documenation",
   base: "/nixos/",
   themeConfig: {
-
     // Assumes GitHub. Can also be a full GitLab url.
     repo: 'dkotrada/nixos',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Contribute!',
+
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/nested/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
 
   }
 }
