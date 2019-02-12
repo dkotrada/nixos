87c0089b997dc91f756477554a394eb49bd4f4f8
Dieter Konrad <dkotrada@gmail.com>
2019-02-12 21:36:10 +0100

    config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,30 +9,6 @@ module.exports = {
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Contribute!',
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/nested/' },
-      { text: 'External', link: 'https://google.com' },
 
-      {
-        text: 'Languages',
-        items: [
-          { text: 'Group1', items: [/*  */] },
-          { text: 'Group2', items: [/*  */] }
-        ]
-      },
-      {
-        text: 'Sprache',
-        items: [
-          { text: 'Group1', items: [/*  */] },
-          { text: 'Group2', items: [/*  */] }
-        ]
-      },
-    ],
-    sidebar: [
-      '/',
-      '/de',
-      ['/nested', 'Explicit link text']
-    ]
   }
 }
--- de/README.md
+++ de/README.md
@@ -22,3 +22,31 @@ export default {
 ```
 
 {{ 1 + 41 }}
+
+```json
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/nested/' },
+      { text: 'External', link: 'https://google.com' },
+
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Group1', items: [/*  */] },
+          { text: 'Group2', items: [/*  */] }
+        ]
+      },
+      {
+        text: 'Sprache',
+        items: [
+          { text: 'Group1', items: [/*  */] },
+          { text: 'Group2', items: [/*  */] }
+        ]
+      },
+    ],
+    sidebar: [
+      '/',
+      '/de',
+      ['/nested', 'Explicit link text']
+    ]
+    ```
