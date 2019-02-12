59922a25c65f04f47000c434a9eaaa5a1340db20
Dieter Konrad <dkotrada@gmail.com>
2019-02-12 21:18:44 +0100

    NAVBAR

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,28 @@
 module.exports = {
   title: "Nix Package Manager",
   description: "Nix Package Manger - User friendly documenation",
-  base: "/nixos/"
+  base: "/nixos/",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Group1', items: [/*  */] },
+          { text: 'Group2', items: [/*  */] }
+        ]
+      },
+      {
+        text: 'Sprache',
+        items: [
+          { text: 'Group1', items: [/*  */] },
+          { text: 'Group2', items: [/*  */] }
+        ]
+      },
+
+    ]
+  }
 }
